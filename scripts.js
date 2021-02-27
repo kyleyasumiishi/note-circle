7933608639e511c5810343ae7e5d2a2f625c2334
Kyle <k.yasumiishi@gmail.com>
2021-02-27 15:12:24 -0800

    Added note logic to scripts

--- scripts.js
+++ scripts.js
@@ -1,10 +1,11 @@
 const noteCircle = [["A","A"], ["A#","Bb"], ["B","B"], ["C","C"], ["C#","Db"], ["D","D"], ["D#","Eb"], ["E","E"], ["F","F"], ["F#","Gb"], ["G","G"], ["G#","Ab"]];
 
-// console.log(noteCircle);
-// console.log(noteCircle.length);
-// for (let i=0; i < noteCircle.length; i++) {
-//     console.log(i);
-// }
+// Button Clicks
+
+
+
+
+// LOGIC
 
 function randomNote() {
     const pitch = Math.floor(Math.random() * noteCircle.length);
@@ -12,6 +13,55 @@ function randomNote() {
     return noteCircle[pitch][enharmonic];
 }
 
-for (let i = 0; i < 100; i++) {
-    randomNote();
+function findPitchPosition(note) {
+    for (let i = 0; i < noteCircle.length; i++) {
+        if (noteCircle[i].includes(note)) {
+            return i; 
+        };
+    }
 }
+
+function findNoteDistance(note1, note2) {
+    const note1Position = findPitchPosition(note1);
+    const note2Position = findPitchPosition(note2);
+    const distance = Math.abs(note1Position - note2Position);
+    return Math.min(12 - distance, distance);
+}
+
+// TESTS 
+
+function test_findPitchPosition () {
+    console.log(findPitchPosition("A"));
+    console.log(findPitchPosition("A#"));
+    console.log(findPitchPosition("Bb"));
+    console.log(findPitchPosition("B"));
+    console.log(findPitchPosition("C"));
+    console.log(findPitchPosition("C#"));
+    console.log(findPitchPosition("Db"));
+    console.log(findPitchPosition("D"));
+    console.log(findPitchPosition("D#"));
+    console.log(findPitchPosition("Eb"));
+    console.log(findPitchPosition("E"));
+    console.log(findPitchPosition("F"));
+    console.log(findPitchPosition("F#"));
+    console.log(findPitchPosition("Gb"));
+    console.log(findPitchPosition("G"));
+    console.log(findPitchPosition("G#"));
+    console.log(findPitchPosition("Ab"));
+}
+
+function test_findNoteDistance() {
+    console.log(findNoteDistance("A","D") == 5);
+    console.log(findNoteDistance("A","C#") == 4);
+    console.log(findNoteDistance("G","E") == 3);
+    console.log(findNoteDistance("Bb","Ab") == 2);
+    console.log(findNoteDistance("D#","Eb") == 0);
+    console.log(findNoteDistance("G","C") == 5);
+    console.log(findNoteDistance("G","B") == 4);
+    console.log(findNoteDistance("D","A#") == 4);
+    console.log(findNoteDistance("F#","C") == 6);
+    console.log(findNoteDistance("E","F") == 1);
+    console.log(findNoteDistance("C","B") == 1);
+    console.log(findNoteDistance("A","A") == 0);
+}
+
